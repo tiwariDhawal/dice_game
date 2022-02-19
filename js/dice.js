bc680c58ef322f4bb9a51ababd3090c10eb5c166
tiwariDhawal <98008402+tiwariDhawal@users.noreply.github.com>
2022-02-19 16:45:24 +0530

    updated

---
+++ js/dice.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber);
+document
+  .querySelector(".img1")
+  .setAttribute("src", "/images/dice" + randomNumber + ".png");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document
+  .querySelector(".img2")
+  .setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+if (randomNumber > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+} else if (randomNumber < randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw🙃";
+}
